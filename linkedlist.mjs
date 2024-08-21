0b72f64e802d554c679f8591ede24922b85ef478
Heero <techheero@gmail.com>
2024-08-21 00:48:44 -0500

    head tail added TDD keeps going

--- linkedlist.mjs
+++ linkedlist.mjs
@@ -1,4 +1,22 @@
+import { Node } from "./node.mjs"
+
 export class Linkedlist{
+    constructor(){
+        this.head = new Node();
+    }
+
+    gethead(){
+        return this.head;
+    }
+
+    tail(){
+        let curr = this.head;
+        while (curr.next !=null){
+            curr = curr.next;
+        }
+        return curr
+    }
+
     length(){
         return 0
     }
