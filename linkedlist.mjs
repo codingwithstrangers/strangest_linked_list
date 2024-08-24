3677f4e176554fcc3f5c2f4b2d641f6195d9f507
Heero <techheero@gmail.com>
2024-08-23 20:34:43 -0500

    test two works need to study more

--- linkedlist.mjs
+++ linkedlist.mjs
@@ -1,25 +1,38 @@
-import { Node } from "./node.mjs"
+import { Node } from "./node.mjs";  // Make sure this path is correct based on your file structure
 
-export class Linkedlist{
-    constructor(){
-        this.head = new Node();
-    }
+export class Linkedlist {
+  constructor() {
+    this.head = null; // Initially, head is null
+    this.tail = null; // Initially, tail is null
+    this.size = 0;    // Initially, size is 0
+  }
 
-    gethead(){
-        return this.head;
-    }
+  // Method to get the head node
+  gethead() {
+    return this.head;
+  }
 
-    tail(){
-        let curr = this.head;
-        while (curr.next !=null){
-            curr = curr.next;
-        }
-        return curr
-    }
+  // Method to get the tail node
+  gettail() {
+    return this.tail;
+  }
 
-    length(){
-        return 0
-    }
-        
-    }
+  // Method to return the current length of the linked list
+  length() {
+    return this.size;
+  }
 
+  // A method to add a node to the linked list
+  add(data) {
+    const newNode = new Node(data); // Create a new node with the given data
+    if (this.head === null) {
+      this.head = newNode; // The first node is the head
+      this.tail = newNode; // Initially, head and tail are the same
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.size++;
+  }
+}
--- test/linked_list.js
+++ test/linked_list.js
@@ -19,16 +19,57 @@ describe("Linkedlist", function () {
       //   assert.equal([1, 2, 3].indexOf(4), -1);
       assert.strictEqual(count, 0);
     });
-    describe("Actualnumber()", function () {
-        it("should return # if linked list is not empty", function () {
-          // given: a new linkelist
-          const num_linkedlist = new Linkedlist();
-          // when: Actualnumber() is run
-          const count = num_linkedlist.length();
-          // then:alert to give total number in linkedlist which should be 0
-          //   assert.equal([1, 2, 3].indexOf(4), -1);
-          assert.notStrictEqual(count,0);
-        });
+
+    describe("findthehead()", function () {
+      it("look in a linked list and find the first node also know as the head", function () {
+    // given: a new empty linked list
+    const new_linkedlist = new Linkedlist();
+
+    // Add a node to the list
+    new_linkedlist.add("first node data");
+
+    // when: calling gethead() it should find the head node
+    const head_node = new_linkedlist.gethead();
+    
+    // then: assert the head node is not null
+    assert.notStrictEqual(head_node, null);
+
+    // Since there is only one node, the head and the tail should be the same
+    const tail_node = new_linkedlist.gettail();
+    assert.strictEqual(head_node, tail_node, "Head and tail should be the same when there's only one node");
+
+    // Add another node to test for head and tail distinction
+    new_linkedlist.add("second node data");
+
+    // Now the head and tail should be different
+    assert.notStrictEqual(new_linkedlist.gethead(), new_linkedlist.gettail(), "Head and tail should be different when there are multiple nodes");
+        
+      });
+
     });
+
+    // describe("findthetail()", function () {
+    //   it("look in a linked list and find the first node also know as the head", function () {
+    //     // given: a new empty linkelist
+    //     const new_linkedlist = new Linkedlist();
+    //     // when calling gethead() it will find the head node
+    //     const head_node = new_linkedlist.gethead();
+    //     // the head node cant be the tail test for this 
+    //     assert.notStrictEqual(head_node,null)
+        
+    //   });
+
+    // });
+    // describe("Actualnumber()", function () {
+    //     it("should return # if linked list is not empty", function () {
+    //       // given: a new linkelist
+    //       const num_linkedlist = new Linkedlist();
+    //       // when: Actualnumber() is run
+    //       const count = num_linkedlist.length();
+    //       // then:alert to give total number in linkedlist which should be 0
+    //       //   assert.equal([1, 2, 3].indexOf(4), -1);
+    //       assert.notStrictEqual(count,0);
+    //     });
+    // });
     });
 });
